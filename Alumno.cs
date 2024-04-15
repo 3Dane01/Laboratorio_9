775dd8511359fd0ea2b5ae0fc7053d94c3143917
Julia Dane <julia_chojolan@umes.edu.gt>
2024-04-14 22:03:34 -0600

    Agregar archivos de proyecto.

---
+++ Alumno.cs
@@ -0,0 +1,10 @@
+﻿namespace Laboratorio_9
+{
+    public class Alumno
+    {
+        public string Dpi { get; set; } 
+        public string Nombre { get; set;}
+        public string Direccion { get; set;}
+
+    }
+}
---
+++ Taller.cs
@@ -0,0 +1,14 @@
+﻿namespace Laboratorio_9
+{
+    public class Taller
+    {
+        string codigo;
+        string nombre;
+        decimal precio;
+
+        public string Codigo { get => codigo; set => codigo = value; }
+        public string Nombre { get => nombre; set => nombre = value; }
+        public decimal Precio { get => precio; set => precio = value; }
+
+    }
+}
