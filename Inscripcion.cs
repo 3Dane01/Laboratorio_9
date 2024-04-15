ac2304cb82d254fa9d0b72f1c4bdf281f6e71f73
Julia Dane <julia_chojolan@umes.edu.gt>
2024-04-15 09:26:16 -0600

    Adelanto

---
+++ Inscripcion.cs
@@ -0,0 +1,13 @@
+﻿namespace Laboratorio_9
+{
+    public class Inscripcion
+    {
+        string codigo;
+        string dpi;
+        DateTime fecha;
+
+        public string Codigo { get => codigo; set => codigo = value; }
+        public string Dpi { get => dpi; set => dpi = value; }
+        public DateTime Fecha { get => fecha; set => fecha = value; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
+using Blazored.LocalStorage;
 using Laboratorio_9;
+
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -7,5 +9,5 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
+builder.Services.AddBlazoredLocalStorage();
 await builder.Build().RunAsync();
